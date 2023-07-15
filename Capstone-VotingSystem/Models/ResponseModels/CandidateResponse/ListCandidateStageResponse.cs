cf99a0195fd4eac061521a43f98c9a5703d0911a
huynh <huynh@DESKTOP-NNLOED3>
2023-07-15 20:20:23 +0700

    fix candidate by stage api

--- Capstone-VotingSystem/Models/ResponseModels/CandidateResponse/ListCandidateStageResponse.cs
+++ Capstone-VotingSystem/Models/ResponseModels/CandidateResponse/ListCandidateStageResponse.cs
@@ -5,7 +5,7 @@
         public Guid CandidateId { get; set; }
         public string? Description { get; set; }
         public string? UserId { get; set; }
-        public Guid? CampaignId { get; set; }
+
         public Guid? GroupId { get; set; }
         public string? FullName { get; set; }
         public string? Phone { get; set; }
